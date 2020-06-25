f684e1bed948c3de32943464a6dce5dc4c2a93b9
jwest <john.westbrook@rcsb.org>
2020-06-25 12:32:39 -0400

    Fix linting issue

--- rcsb/utils/struct/ScopClassificationProvider.py
+++ rcsb/utils/struct/ScopClassificationProvider.py
@@ -33,7 +33,7 @@ class ScopClassificationProvider(object):
         useCache = kwargs.get("useCache", True)
         urlTarget = kwargs.get("scopTargetUrl", "http://scop.berkeley.edu/downloads/update")
         # self.__version = kwargs.get("scopVersion", "2.07-2019-07-23")
-        #self.__version = kwargs.get("scopVersion", "2.07-2020-01-23")
+        # self.__version = kwargs.get("scopVersion", "2.07-2020-01-23")
         self.__version = kwargs.get("scopVersion", "2.07-2020-05-07")
         #
         self.__mU = MarshalUtil(workPath=self.__scopDirPath)
